1f92bcc37ae42f02d0a7e1e29ed78f6627217c4d
kartikay1361 <58590534+kartikay1361@users.noreply.github.com>
2019-12-06 17:04:58 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main() {
-    int i, n, t1 = 0, t3 = 1, nextTerm;
+    int i, n, t1 = 0, t2 = 1, nextTerm;
     printf("Enter the number of terms: ");
     scanf("%d", &n);
     printf("Fibonacci Series: ");
 
     for (i = 1; i <= n; ++i) {
-        printf("%d, ", t4);
+        printf("%d, ", t2);
         nextTerm = t1 + t2;
         t1 = t2;
         t2 = nextTerm;
